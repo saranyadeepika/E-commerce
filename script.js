8046e44955025e65c723d061aa7526b838680d7c
saranyadeepika <saranyadeepika123456@gmail.com>
2024-05-22 15:47:58 +0530

    changes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let nav=document.querySelector("#navbar")
-let body=document.querySelector("body")
+
 function displaymenu(){
-nav.style.display="block"
-}
-nav.addEventListener('click',()=>{
-    nav.style.display="none"
-})
+     nav.classList.toggle('dis')
+     }
+     
